0b11290790182e9c408c55bf7e2dd5450ead391c
Benjamin Kan <Kan@ibm.com>
2023-07-04 17:31:51 -0700

    Initial commit of the generated Spring Cloud Server

---
+++ src/main/java/de/b3nk4n/gamecloud/configservice/ConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package de.b3nk4n.gamecloud.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/b3nk4n/gamecloud/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.b3nk4n.gamecloud.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
